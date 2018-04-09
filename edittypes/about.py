c9cfe3b8003db33201d807db73f5cf989a42dd01
Sumit Asthana <asthana.sumit23@gmail.com>
2018-04-09 22:24:16 +0530

    fix name in about

--- edittypes/about.py
+++ edittypes/about.py
@@ -1,7 +1,7 @@
-__name__ = "drafttopic"
+__name__ = "edittypes"
 __version__ = "0.1.1"
-__author__ = ["Aaron Halfaker"]
-__author_email__ = ["ahalfaker@wikimedia.org"]
+__author__ = ["Aaron Halfaker", "Sumit Asthana"]
+__author_email__ = ["ahalfaker@wikimedia.org", "asthana.sumit23@gmail.com"]
 __description__ = "A library for automatic detection of edittypes of edits " +\
                   "on Wikipedia based on semantic edit intentions"
 __url__ = "https://github.com/wiki-ai/edittypes"
